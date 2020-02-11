e9d53292f9e15f2923f000e9e7da3e25333050d3
wangyiqiang <364514921@qq.com>
2020-02-12 06:07:08 +0800

    Thor Study

---
+++ java-demo/src/test/java/org/thor/javademo/JavaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.thor.javademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
