61da93420e19d920a1b46841d9a8abdace377251
wangyiqiang <364514921@qq.com>
2020-02-12 06:22:31 +0800

    新增秒杀模块

---
+++ miaosha/src/main/java/org/thor/miaosha/MiaoshaApplication.java
@@ -0,0 +1,13 @@
+package org.thor.miaosha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiaoshaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiaoshaApplication.class, args);
+	}
+
+}
---
+++ miaosha/src/test/java/org/thor/miaosha/MiaoshaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.thor.miaosha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiaoshaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
